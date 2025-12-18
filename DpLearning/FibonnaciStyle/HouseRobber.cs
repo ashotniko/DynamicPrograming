bd916a89624ad05720527097a1d00a9f3226adcc
ashotn <Ashot.Nikoyan@evoca.am>
2025-12-18 13:10:36 +0400

    Added HouseRobber.cs MinCostClimbingStairs.cs files

---
+++ DpLearning/FibonnaciStyle/HouseRobber.cs
@@ -0,0 +1,36 @@
+﻿namespace DpLearning.FibonnaciStyle
+{
+    //You are a professional robber planning to rob houses along a street.
+    //Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that 
+    //adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were
+    //broken into on the same night.
+    //Given an integer array nums representing the amount of money of each house, return the maximum amount of money you
+    //can rob tonight without alerting the police.
+    internal class HouseRobber
+    {
+        public int Rob(int[] nums)
+        {
+            var numsLength = nums.Length;
+
+            if (numsLength == 1)
+            {
+                return nums[0];
+            }
+
+            var dpHelper = new int[numsLength];
+
+            dpHelper[0] = nums[0];
+            dpHelper[1] = Math.Max(nums[0], nums[1]);
+
+            for (var i = 2; i < numsLength; i++)
+            {
+                dpHelper[i] = Math.Max(
+                    dpHelper[i - 1],
+                    dpHelper[i - 2] + nums[i]
+                );
+            }
+
+            return dpHelper[numsLength - 1];
+        }
+    }
+}
---
+++ DpLearning/FibonnaciStyle/MinCostClimbingStairs.cs
@@ -0,0 +1,25 @@
+﻿namespace DpLearning.FibonnaciStyle
+{
+    //You are given an integer array cost where cost[i] is the cost of ith step on a staircase.Once you pay the cost, you can either climb one or two steps.
+    //You can either start from the step with index 0, or the step with index 1.
+    //Return the minimum cost to reach the top of the floor.
+    internal class MinCostClimbingStairs
+    {
+        public int MinimalCostClimbingStairs(int[] cost)
+        {
+            var costLenght = cost.Length;
+            var dpHelper = new int[costLenght + 1];
+
+            dpHelper[0] = dpHelper[1] = 0;
+
+            for (var i = 2; i < costLenght; i++)
+            {
+                dpHelper[i] = Math.Min(
+                    dpHelper[i - 1] + cost[i - 1],
+                    dpHelper[i - 2] + cost[i - 2]
+                );
+            }
+            return dpHelper[costLenght];
+        }
+    }
+}
