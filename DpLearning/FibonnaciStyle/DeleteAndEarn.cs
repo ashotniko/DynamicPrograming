b49c5596b37495f56ef44b0af734dd54b8e673a3
ashotn <Ashot.Nikoyan@evoca.am>
2025-12-19 17:32:16 +0400

    Edit cs files, add DeleteAndEarn.cs file

---
+++ DpLearning/FibonnaciStyle/DeleteAndEarn.cs
@@ -0,0 +1,51 @@
+﻿namespace DpLearning.FibonnaciStyle
+{
+    //You are given an integer array nums. You want to maximize the number of points you get by performing the following operation
+    //any number of times:
+    //Pick any nums[i] and delete it to earn nums[i] points.Afterwards, you must delete every element e
+    //qual to nums[i] - 1 and every element equal to nums[i] + 1.
+    //Return the maximum number of points you can earn by applying the above operation some number of times.
+    internal class DeleteAndEarn
+    {
+        public static int DelAndEarn(int[] nums)
+        {
+            var max = GetMax(nums);
+            var values = CreateDpValues(nums, max);
+
+            var take = 0;
+            var skip = 0;
+
+            for (var i = 0; i <= max; i++)
+            {
+                var takei = skip + values[i];
+                var skipi = Math.Max(skip, take);
+                take = takei;
+                skip = skipi;
+            }
+
+            return Math.Max(take, skip);
+        }
+
+        private static int GetMax(int[] nums)
+        {
+            var max = 0;
+            foreach (var num in nums)
+            {
+                max = Math.Max(max, num);
+            }
+            return max;
+        }
+
+        private static int[] CreateDpValues(int[] nums, int length)
+        {
+            var values = new int[length + 1];
+
+            foreach (var num in nums)
+            {
+                values[num] += num;
+            }
+
+            return values;
+        }
+    }
+}
